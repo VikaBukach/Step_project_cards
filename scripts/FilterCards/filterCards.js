fb7afec026758c427fdecf9e3e6eea6d00551bb6
Victoria <bukachviko@gmail.com>
2023-11-24 14:31:08 +0200

    make searchform

--- scripts/FilterCards/filterCards.js
+++ scripts/FilterCards/filterCards.js
@@ -14,16 +14,17 @@ export function CreateFormFilter() {
     <div class= "search__form-wrapper" >
         <form class ="search__form">
             <input type="text" name="search" id="search" placeholder="Введіть прізвище">
-                <select name="" id="status">
-                    <option value="all">All</option>
-                    <option value="open">Open</option>
-                    <option value="done">Done</option>
+                <select id="doctors">
+                    <option value="choose">Обрати лікаря</option>
+                    <option value="DentistVisit">Стоматолог</option>
+                    <option value="TherapistVisit">Терапевт</option>
+                    <option value="CardioVisit">Кардіолог</option>
                 </select>
-                <select name="" id="urgency">
-                    <option value="all">All</option>
-                    <option value="hign">High</option>
-                    <option value="norm">Normal</option>
-                    <option value="low">Low</option>
+                <select id="urgency">
+                    <option value="all">Терміновість</option>   
+                    <option value="ordinary">Звичайна</option>
+                    <option value="priority">Пріорітетна</option>
+                    <option value="low">Невідкладна</option>
                 </select>
         </form>
     </div>
@@ -45,5 +46,19 @@ document.addEventListener("input", (event) => {
     if(event.target.id === "search") {
         renderCards(filterCards(event.target.value))
         // console.log(filterCards(event.target.value))
+    }
+})
+
+function filterDoctors(event) {
+
+    return arrCardsVisits.filter((card) => {
+        return card.doctors.includes(event)
+    })
+}
+document.addEventListener("change", (event) => {
+    console.log(event.target)
+    if(event.target.id === "doctors") {
+        console.log(event.target.value)
+
     }
 })
